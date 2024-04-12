745782c028c52173ee80e42b5a78496a923efaca
amit-meerchandani <meerchandaniamit@gmail.com>
2024-04-12 10:43:23 +0530

    fix bug

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -5,22 +5,23 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
+  // const { email, subject, message } = await req.json();
+  // console.log(email, subject, message);
   try {
-    const data = await resend.emails.send({
-      from: fromEmail,
-      to: [fromEmail, email],
-      subject: subject,
-      react: (
-        <>
-          <h1>{subject}</h1>
-          <p>Thank you for contacting us!</p>
-          <p>New message submitted:</p>
-          <p>{message}</p>
-        </>
-      ),
-    });
+    // const data = await resend.emails.send({
+    //   from: fromEmail,
+    //   to: [fromEmail, email],
+    //   subject: subject,
+    //   react: (
+    //     <>
+    //       <h1>{subject}</h1>
+    //       <p>Thank you for contacting us!</p>
+    //       <p>New message submitted:</p>
+    //       <p>{message}</p>
+    //     </>
+    //   ),
+    // });
+    const data = 'working';
     console.log('data', data);
     return NextResponse.json(data);
   } catch (error) {
